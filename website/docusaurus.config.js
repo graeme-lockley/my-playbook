229db93492f378555092f0f9efae5ce71b9d4091
Graeme Lockley <graeme.lockley@gmail.com>
2022-04-17 12:50:09 +0200

    doc: change tutorial to playbook

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Playbook',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
@@ -70,7 +70,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Playbook',
                 to: '/docs/intro',
               },
             ],
