f785cdf495bc854913447da0b6d2a34c9bdc0f89
Graeme Lockley <graeme.lockley@gmail.com>
2022-04-30 07:32:05 +0000

    fix: update exclusions

--- scripts/sca-node-dependencies.sh
+++ scripts/sca-node-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_HOME_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-EXCLUSIONS=1068044,1067654
+EXCLUSIONS=1067654,1070259
 
 (
     cd "$SCRIPT_HOME_DIR"/../website || exit 1
